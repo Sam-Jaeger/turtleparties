4ceebe752d929639cb87d9c637fa9dafd3716d27
Sam-Jaeger <shjaeger@ualberta.ca>
2022-02-28 17:05:49 -0700

    added Player Class and Firebase dependencies

---
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/Player.java
@@ -0,0 +1,33 @@
+package com.example.turtlepartiesapp;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class Player {
+    FirebaseFirestore db;
+    //QRGenerator qrGenerator;
+
+    int id;
+    int score;
+    //ArrayList<QRCode> qrCodes;
+
+    public Player(int id) {
+        //use db to get my stuff
+    }
+
+//    public Player(int id, int score, ArrayList<QRCode> qrCodes) {
+//        this.id = id;
+//        this.score = score;
+//        this.qrCodes = qrCodes;
+//    }
+//
+////    public ArrayList<QRCode> getQRCodes() {
+////        return this.qrCodes;
+////    }
+////
+////    public void AddQRCode(QRCode qrCode){
+////
+////    }
+
+}
